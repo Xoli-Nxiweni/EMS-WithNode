cceb04020bb55e769dc275795481ccaf119c4601
Xoli <xolinxiweni@gmail.com>
2024-11-21 12:14:06 +0200

    removed cors things that makes the application not render on other port numbers

--- Backend/server.js
+++ Backend/server.js
@@ -21,7 +21,6 @@ const bucket = admin.storage().bucket();
 const app = express();
 // Update CORS middleware to allow credentials
 app.use(cors({
-  origin: 'http://localhost:5173',
   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true // Allow credentials
